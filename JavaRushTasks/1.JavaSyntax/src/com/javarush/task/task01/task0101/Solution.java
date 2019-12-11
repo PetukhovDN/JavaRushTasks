5da85c2879ea371088b748c7fc1ca93453cc5826
PetukhovDN <petuhovdn77112@mail.ru>
2019-12-11 19:09:28 +0600

    Another try

--- JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task01/task0101/Solution.java
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task01/task0101/Solution.java
@@ -6,6 +6,6 @@ package com.javarush.task.task01.task0101;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println("Я думаю, быть программистом - это круто");//напишите тут ваш код
+        System.out.println("Я думаю, быть программистом - это очень круто");//напишите тут ваш код
     }
 }
