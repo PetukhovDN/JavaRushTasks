198197d23ab600955875fa7094f9202cd7e3fa66
PetukhovDN <petuhovdn77112@mail.ru>
2019-12-14 09:43:09 +0600

    Second Day

---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task01/task0110/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task01.task0110;
+
+/* 
+На нашем экране — переменная
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String name = "Веталь";//напишите тут ваш код
+        System.out.println(name);
+    }
+
+}
