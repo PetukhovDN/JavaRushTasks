53e89c048a0a6d15ba1f46aa8a6ef07138d9cf98
PetukhovDN <petuhovdn77112@mail.ru>
2019-12-14 11:40:39 +0600

    Second Day

---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task01/task0138/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task01.task0138;
+
+/* 
+Любимое стихотворение
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Мое любимое стихотворение:");
+        System.out.println();
+        System.out.println("Скажи как дядя ведь недаром");
+        System.out.println("Москва, спаленная пожаром");
+        System.out.println("Фрацузу отдана?");
+        System.out.println("Ведь были битвы боевые");
+        System.out.println("Да говорят еще какие!");
+        //напишите тут ваш код
+    }
+}
