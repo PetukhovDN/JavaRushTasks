7ee0db9f1140e1d4ece56bfe15252b5c415584ad
PetukhovDN <petuhovdn77112@mail.ru>
2019-12-18 10:12:28 +0600

    Third Day

--- JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task04/task0414/Solution.java
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task04/task0414/Solution.java
@@ -4,12 +4,21 @@ package com.javarush.task.task04.task0414;
 Количество дней в году
 */
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
-
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));//напишите тут ваш код
+        int a = Integer.parseInt(reader.readLine());
+        int x;
+        if (a % 4 == 0) {
+            if (a % 100 == 0) {
+                if (a % 400 == 0) {
+                    x = 366;
+                } else x = 365;
+            } else x = 366;
+        } else x = 365;
+        System.out.println("количество дней в году: " + x);
     }
 }
---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task04/task0416/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task04.task0416;
+
+/* 
+Переходим дорогу вслепую
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+
+    }
+}
