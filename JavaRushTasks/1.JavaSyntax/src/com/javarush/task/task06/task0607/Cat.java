8db9deaa2f442e780b8e6569152bcee3d38374b8
PetukhovDN <petuhovdn77112@mail.ru>
2019-12-22 13:26:16 +0600

    Sixth day

---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task06/task0607/Cat.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task06.task0607;
+
+/* 
+Классовый счетчик
+*/
+
+public class Cat {
+    private static int catCount = 0;//напишите тут ваш код
+
+    public Cat() {
+        catCount++;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
