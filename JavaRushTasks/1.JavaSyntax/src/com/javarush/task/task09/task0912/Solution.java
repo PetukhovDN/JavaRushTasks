bc4328d8202105b737a7828e96ce06b6fbad8376
Dmitriy Petukhov <petuhovdn77112@mail.ru>
2019-12-27 17:56:34 +0600

    Ninth day

---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task09/task0912/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task09.task0912;
+
+/* 
+Исключение при работе с числами
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        try {
+            int num = Integer.parseInt("XYZ");
+            System.out.println(num);
+        }
+        catch(NumberFormatException e){
+            System.out.print(e.getClass());
+        }
+
+        //напишите тут ваш код
+    }
+}
