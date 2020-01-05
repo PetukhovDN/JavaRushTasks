80723b5e93e22c73d1940466d34afe20a6e9daa3
Dmitriy Petukhov <petuhovdn77112@mail.ru>
2020-01-05 15:08:02 +0600

    Thirteenth day

--- JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task09/task0926/Solution.java
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task09/task0926/Solution.java
@@ -13,7 +13,19 @@ public class Solution {
     }
 
     public static ArrayList<int[]> createList() {
-        //напишите тут ваш код
+        ArrayList<int[]> list = new ArrayList<>();
+        int[] int1 = new int[]{3, 6, 34, 8, 1};
+        int[] int2 = new int[]{23, 67};
+        int[] int3 = new int[]{12, 56, 89, 234};
+        int[] int4 = new int[]{1, 2, 3, 4, 5, 6, 7};
+        int[] int5 = new int[]{};
+        list.add(int1);
+        list.add(int2);
+        list.add(int3);
+        list.add(int4);
+        list.add(int5);
+
+        return list;
     }
 
     public static void printList(ArrayList<int[]> list) {
