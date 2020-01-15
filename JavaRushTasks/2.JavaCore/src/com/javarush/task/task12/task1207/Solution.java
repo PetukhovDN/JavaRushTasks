f95d2b91c5166dbaf70bfa093550a8146f8f02aa
Dmitriy Petukhov <petuhovdn77112@mail.ru>
2020-01-15 15:43:02 +0600

    Sixteenth day

---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int x = 9;
+        print(x);
+        print(Integer.valueOf(x));
+    }
+
+    public static void print(int a) {
+    }//Напишите тут ваши методы
+
+    public static void print(Integer a) {
+    }
+}
---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print (int a){}
+    public static void print (String a){}
+    public static void print (boolean a){}
+    public static void print (Boolean a){}
+    public static void print (char a){}
+    //Напишите тут ваши методы
+}
