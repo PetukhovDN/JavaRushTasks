66b39322038f221e028e3305ea86d3ca83a7b16d
Dmitriy Petukhov <petuhovdn77112@mail.ru>
2020-01-16 08:47:14 +0600

    Seventeenth day

---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task12/task1224/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task12.task1224;
+
+/* 
+Неведома зверушка
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getObjectType(new Cat()));
+        System.out.println(getObjectType(new Tiger()));
+        System.out.println(getObjectType(new Lion()));
+        System.out.println(getObjectType(new Bull()));
+        System.out.println(getObjectType(new Pig()));
+    }
+
+    public static String getObjectType(Object o) {
+        return o instanceof Cat ? "Кот" :
+                o instanceof Tiger ? "Тигр" :
+                        o instanceof Lion ? "Лев" :
+                                o instanceof Bull ? "Бык" : "Животное";
+    }
+
+    public static class Cat {
+    }
+
+    public static class Tiger {
+    }
+
+    public static class Lion {
+    }
+
+    public static class Bull {
+    }
+
+    public static class Pig {
+    }
+}
