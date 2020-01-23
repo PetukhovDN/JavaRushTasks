094b09cb8aa3b5f0eb1ff97f46a4adfe8ef3e9de
Dmitriy Petukhov <petuhovdn77112@mail.ru>
2020-01-23 11:04:55 +0600

    Twenty first day

---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task15/task1507/Solution.java
@@ -0,0 +1,65 @@
+package com.javarush.task.task15.task1507;
+
+/* 
+ООП - Перегрузка
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        printMatrix(2, 3, "8");
+    }
+
+    public static void printMatrix(int m, int n, String value) {
+        System.out.println("Заполняем объектами String");
+        printMatrix(m, n, (Object) value);
+    }
+
+    public static void printMatrix(Integer m, int n, String value) {
+        System.out.println("Заполняем объектами String");
+        printMatrix(m, n, (Object) value);
+    }
+
+    public static void printMatrix(Short m, Short n, String value) {
+        System.out.println("Заполняем объектами String");
+        printMatrix(m, n, (Object) value);
+    }
+
+    public static void printMatrix(Integer m, int n, Object value) {
+        System.out.println("Заполняем объектами String");
+        printMatrix(m, n, (Object) value);
+    }
+
+    public static void printMatrix(int m, int n, Integer value) {
+        System.out.println("Заполняем объектами String");
+        printMatrix(m, n, (Object) value);
+    }
+
+    public static void printMatrix(int m, Double n, Object value) {
+        System.out.println("Заполняем объектами String");
+        printMatrix(m, n, (Object) value);
+    }
+
+    public static void printMatrix(int m, int n, Double value) {
+        System.out.println("Заполняем объектами String");
+        printMatrix(m, n, (Object) value);
+    }
+
+    public static void printMatrix(int m, Float n, String value) {
+        System.out.println("Заполняем объектами String");
+        printMatrix(m, n, (Object) value);
+    }
+
+    public static void printMatrix(Short m, int n, Object value) {
+        System.out.println("Заполняем объектами String");
+        printMatrix(m, n, (Object) value);
+    }
+
+    public static void printMatrix(int m, Float n, Object value) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(value);
+            }
+            System.out.println();
+        }
+    }
+}
