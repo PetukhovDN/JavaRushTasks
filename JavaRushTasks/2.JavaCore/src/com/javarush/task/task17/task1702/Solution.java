db201728a00fc411d19a23880a0d0d6c6e05e903
Dmitriy Petukhov <petuhovdn77112@mail.ru>
2020-02-04 19:35:10 +0600

    Twenty seventh day

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task17/task1702/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task17/task1702/Solution.java
@@ -41,7 +41,7 @@ public class Solution {
         }
     }
 
-    public static synchronized void initThreads() throws InterruptedException {
+    public static void initThreads() throws InterruptedException {
         List<Thread> threads = new ArrayList<Thread>(threadCount);
         for (int i = 0; i < threadCount; i++) threads.add(new SortThread());
         for (Thread thread : threads) thread.start();
