a59fa44f70347b7dcfe5539caab63e76060c9b13
Dmitriy Petukhov <petuhovdn77112@mail.ru>
2020-02-28 12:09:19 +0600

    Thirty seventh day

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task20/task2019/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task20/task2019/Solution.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Solution implements Serializable {
 
     public static void main(String args[]) throws Exception {
-        FileOutputStream fileOutput = new FileOutputStream("your.file.name");
+        FileOutputStream fileOutput = new FileOutputStream("your_file_name");
         ObjectOutputStream outputStream = new ObjectOutputStream(fileOutput);
 
         Solution solution = new Solution();
@@ -20,7 +20,7 @@ public class Solution implements Serializable {
         outputStream.close();
 
         //load
-        FileInputStream fiStream = new FileInputStream("your.file.name");
+        FileInputStream fiStream = new FileInputStream("your_file_name");
         ObjectInputStream objectStream = new ObjectInputStream(fiStream);
 
         Solution loadedObject = (Solution) objectStream.readObject();
