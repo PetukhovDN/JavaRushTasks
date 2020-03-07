6bbc8ab2eef1a337eee9b889b054bd8e719ace7c
Dmitriy Petukhov <petuhovdn77112@mail.ru>
2020-03-07 14:18:12 +0600

    The solution of the Minesweeper game

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task20/task2025/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task20/task2025/Solution.java
@@ -14,9 +14,11 @@ import java.util.stream.Stream;
  */
 public class Solution {
     public static long[] getNumbers(long N) {
-        System.out.println(String.valueOf(Long.MAX_VALUE).length());
         List<Long> list = new ArrayList<>();
-        for (int i = 1; i < N; i++) {
+        for (int i = 1; i < 10 ; i++) {
+            list.add((long)i);
+        }
+        for (long i = 10; i < N; i++) {
             int length = String.valueOf(i).length();
             long sumOfDigits = Stream.of(String.valueOf(i)
                     .split(""))
@@ -24,9 +26,13 @@ public class Solution {
                     .map(x -> (int) Math.pow(x, length))
                     .sum();
             if (sumOfDigits == i) {
-                list.add(sumOfDigits);
+                list.add(i);
             }
 
+        }
+        for (long l: list
+             ) {
+            System.out.println(l);
         }
 
         long[] result = new long[list.size()];
@@ -38,6 +44,6 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        getNumbers(999999);
+        getNumbers(9999999);
     }
 }
--- JavaRushTasks/8.Games/src/com/javarush/games/minesweeper/GameObject.java
+++ JavaRushTasks/8.Games/src/com/javarush/games/minesweeper/GameObject.java
@@ -3,8 +3,14 @@ package com.javarush.games.minesweeper;
 public class GameObject {
     public int x;
     public int y;
-    public GameObject(int x, int y){
+    public boolean isMine;
+    public int countMineNeighbors;
+    public boolean isOpen;
+    public boolean isFlag;
+
+    public GameObject(int x, int y, boolean isMine) {
         this.x = x;
         this.y = y;
+        this.isMine = isMine;
     }
 }
